9b5bf9465a01a65a862a8a784768f38f9bcc6d71
RomanTrotman <147647190+RomanTrotman@users.noreply.github.com>
2024-02-16 15:42:35 +0000

    Add files via upload

---
+++ tdx_Launcher.lua
@@ -0,0 +1,11 @@
+if not game:IsLoaded() then
+   game.Loaded:Wait();
+end
+
+queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
+
+if game.PlaceId == 9503261072 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RomanTrotman/freeamongussus/main/tdx_auto_lobby.lua", true))()
+elseif game.PlaceId == 11739766412 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RomanTrotman/freeamongussus/main/tdx_match.lua", true))()
+end
