5f4ee2ade184cae732a98db54f2674ec092eb15e
RomanTrotman <147647190+RomanTrotman@users.noreply.github.com>
2024-02-16 00:12:12 +0000

    Add files via upload

---
+++ tdx_auto_lobby.lua
@@ -0,0 +1,22 @@
+while wait(5) do
+	if game.Workspace.APCs:FindFirstChild("1").mapdisplay.screen.displayscreen.map.Text == "SECRET FOREST" then
+		workspace.Not_WlLLEM233.HumanoidRootPart.CFrame = workspace.APCs["1"].APC.Detector.CFrame
+	elseif game.Workspace.APCs:FindFirstChild("2").mapdisplay.screen.displayscreen.map.Text == "SECRET FOREST" then
+		workspace.Not_WlLLEM233.HumanoidRootPart.CFrame = workspace.APCs["2"].APC.Detector.CFrame
+	elseif game.Workspace.APCs:FindFirstChild("3").mapdisplay.screen.displayscreen.map.Text == "SECRET FOREST" then
+		workspace.Not_WlLLEM233.HumanoidRootPart.CFrame = workspace.APCs["3"].APC.Detector.CFrame
+	elseif game.Workspace.APCs:FindFirstChild("4").mapdisplay.screen.displayscreen.map.Text == "SECRET FOREST" then
+		workspace.Not_WlLLEM233.HumanoidRootPart.CFrame = workspace.APCs["4"].APC.Detector.CFrame
+	elseif game.Workspace.APCs:FindFirstChild("5").mapdisplay.screen.displayscreen.map.Text == "SECRET FOREST" then
+		workspace.Not_WlLLEM233.HumanoidRootPart.CFrame = workspace.APCs["5"].APC.Detector.CFrame
+	elseif game.Workspace.APCs:FindFirstChild("6").mapdisplay.screen.displayscreen.map.Text == "SECRET FOREST" then
+		workspace.Not_WlLLEM233.HumanoidRootPart.CFrame = workspace.APCs["6"].APC.Detector.CFrame
+	elseif game.Workspace.APCs:FindFirstChild("7").mapdisplay.screen.displayscreen.map.Text == "SECRET FOREST" then
+		workspace.Not_WlLLEM233.HumanoidRootPart.CFrame = workspace.APCs["7"].APC.Detector.CFrame
+	elseif game.Workspace.APCs:FindFirstChild("8").mapdisplay.screen.displayscreen.map.Text == "SECRET FOREST" then
+		workspace.Not_WlLLEM233.HumanoidRootPart.CFrame = workspace.APCs["8"].APC.Detector.CFrame
+	else
+		print("there nothing")
+end
+end
+
