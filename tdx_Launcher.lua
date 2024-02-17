8a44805eaf2161d54bb61519fc804bb8f76b55b1
RomanTrotman <147647190+RomanTrotman@users.noreply.github.com>
2024-02-17 12:12:10 +0000

    Update tdx_Launcher.lua

--- tdx_Launcher.lua
+++ tdx_Launcher.lua
@@ -1,6 +1,7 @@
 if not game:IsLoaded() then
    game.Loaded:Wait();
 end
+queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RomanTrotman/freeamongussus/main/tdx_failsafe.lua", true))()
 if game.PlaceId == 9503261072 then
