b593ec2071ab1b87e77a388fafebf9e31b16b295
RomanTrotman <147647190+RomanTrotman@users.noreply.github.com>
2024-02-17 12:15:36 +0000

    Update tdx_failsafe.lua

--- tdx_failsafe.lua
+++ tdx_failsafe.lua
@@ -3,13 +3,19 @@ if not game:IsLoaded() then
 end
 
 if game.PlaceId == 9503261072 then
+   
+print("lobby failsafe")
 wait(300)
 local ts = game:GetService("TeleportService")
 local p = game:GetService("Players").LocalPlayer
 ts:Teleport(9503261072, p)
+   
 elseif game.PlaceId == 11739766412 then
+   
+print("game failsafe")
 wait(1200)
 local ts = game:GetService("TeleportService")
 local p = game:GetService("Players").LocalPlayer
 ts:Teleport(9503261072, p)
+   
 end
