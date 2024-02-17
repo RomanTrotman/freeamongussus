0769b6b2e5e4d1b830066f607343bde9d0c8638e
RomanTrotman <147647190+RomanTrotman@users.noreply.github.com>
2024-02-17 12:04:40 +0000

    Update tdx_Launcher.lua

--- tdx_Launcher.lua
+++ tdx_Launcher.lua
@@ -2,7 +2,6 @@ if not game:IsLoaded() then
    game.Loaded:Wait();
 end
 
-queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/RomanTrotman/freeamongussus/main/tdx_Launcher.lua'))()")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RomanTrotman/freeamongussus/main/tdx_failsafe.lua", true))()
 if game.PlaceId == 9503261072 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RomanTrotman/freeamongussus/main/tdx_auto_lobby.lua", true))()
