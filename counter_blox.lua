15b3e5c714568d5f42ea84785dd05937db5005c7
RomanTrotman <147647190+RomanTrotman@users.noreply.github.com>
2024-03-08 13:31:34 +0000

    Create counter_blox.lua

---
+++ counter_blox.lua
@@ -0,0 +1,8 @@
+local yomama = "very fat"
+local anser = game.Players.localplayer.character.answer
+print("is your mama fat?")
+if anser == true then
+  print("very cool")
+else
+  print("no not true your mother is", yomama)
+end
